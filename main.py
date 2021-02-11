53baa3367822bb87f8fea07a7fa358356853b7ac
Juan Cruz <jc23729@gmail.com>
2021-02-11 02:03:07 +0000

    format data account

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ from game_data import data
 import random
 from art import logo, vs
 from replit import clear
+
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
+
+def format_data(account):
+  """Format account into printable format: name, description and country"""
+  name = account["name"]
+  description = account["description"]
+  country = account["country"]
+  # print(f'{name}: {account["follower_count"]}')
+  return f"{name}, a {description}, from {country}"
+
